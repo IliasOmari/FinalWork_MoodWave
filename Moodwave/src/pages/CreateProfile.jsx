e9256e950533456a3dc9a3ed4fec0a4c4d0d4549
IliasOmari <ilias.omari@student.ehb.be>
2024-05-22 17:38:06 +0200

    added the logic in the profile creation

--- Moodwave/src/pages/CreateProfile.jsx
+++ Moodwave/src/pages/CreateProfile.jsx
@@ -1,35 +1,125 @@
+import { useEffect, useState } from "react";
 import "./createprofile-modules.css";
+import { TbReload } from "react-icons/tb";
+import { createAvatar } from "@dicebear/core";
+import { identicon } from "@dicebear/collection";
+import Select from "react-select";
+import Genres from "./genres.json";
+import { BeatLoader } from "react-spinners";
+import { Navigate, useLoaderData } from "react-router-dom";
+
 const CreateProfile = () => {
+  const user = useLoaderData();
+  const [inputs, setInputs] = useState({ username: "", genres: [] });
+  const [isLoading, setIsLoading] = useState(false);
+  const [svg, setSvg] = useState("");
+  const [change, setChange] = useState(false);
+  useEffect(() => {
+    async function getUri() {
+      const avatar = createAvatar(identicon, {
+        seed: Math.random(),
+        radius: "50%",
+        backgroundType: ["gradientLinear", "solid"],
+      });
+      const svg = await avatar.toDataUri();
+      setSvg(svg);
+    }
+
+    getUri();
+  }, [change]);
+
+  const createProfile = () => {
+    if (!svg || inputs.genres.length === 0 || !inputs.username) {
+      alert("Please fill in the missing fields");
+      return;
+    }
+
+    setIsLoading(true);
+
+    fetch("http://localhost:3000/updateProfile", {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        genres: inputs.genres,
+        username: inputs.username,
+        image: svg,
+        id: localStorage.getItem("id") ?? null,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          setIsLoading(false);
+          alert(data.error);
+        }
+        window.location.href = "/profile";
+      });
+  };
+
   return (
     <>
-      <div className="background">
-        <div className="title-create">
-          <h1>Create your profile </h1>
-        </div>
-        <div className="avatar">
-          <img src="#" alt="" />
-        </div>
-        <div className="profile-inputs">
-          <label className="label_username" htmlFor="username">
-            Username
-          </label>
-          <input type="text" name="username" id="username" />
+      {!user ? (
+        <Navigate to="/login" />
+      ) : user?.username ? (
+        <Navigate to="/profile" />
+      ) : (
+        <>
+          {" "}
+          <div className="background">
+            <div className="title-create">
+              <h1>Create your profile </h1>
+            </div>
 
-          <label className="label_username" htmlFor="username">
-            Birthday {"(optional)"}
-          </label>
-          <input type="date" name="birthday" id="birthday" />
+            <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+              <img src={svg} className="avatar" />
+              <TbReload
+                color="white"
+                size={20}
+                onClick={() => setChange((prev) => !prev)}
+              />
+            </div>
+            <div className="profile-inputs">
+              <label className="label_username" htmlFor="username">
+                Username
+              </label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                onChange={(e) =>
+                  setInputs((prev) => ({ ...prev, username: e.target.value }))
+                }
+              />
+              <label className="label_username" htmlFor="username">
+                Favourite music genres
+              </label>
+              <Select
+                onChange={(e) => setInputs((prev) => ({ ...prev, genres: e }))}
+                isMulti
+                styles={{
+                  container: (baseStyles) => ({
+                    ...baseStyles,
+                    color: "black",
+                    width: "95%",
+                  }),
+                }}
+                options={Genres.map((el) => {
+                  return { value: el, label: el };
+                })}
+              />
+            </div>
 
-          <label className="label_username" htmlFor="username">
-            Favourite music genres
-          </label>
-          <input type="text" name="music-genres" id="music-genres" />
-        </div>
-
-        <div className="button">
-          <button className="submit">Save changes</button>
-        </div>
-      </div>
+            <div className="button">
+              <button className="submit" onClick={() => createProfile()}>
+                {isLoading ? <BeatLoader color="#ffff" /> : "Save changes"}
+              </button>
+            </div>
+          </div>{" "}
+        </>
+      )}
     </>
   );
 };
