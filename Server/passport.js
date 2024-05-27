e2c30cb7e76353e31e7b6c4a77854426e63b86dc
IliasOmari <ilias.omari@student.ehb.be>
2024-05-27 18:11:51 +0200

    Update passport.js

--- Server/passport.js
+++ Server/passport.js
@@ -8,7 +8,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL:
+        "https://finalwork-moodwave-api.onrender.com/auth/google/callback",
     },
     async function (accessToken, refreshToken, profile, done) {
       try {
